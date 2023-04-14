60767949ac8cbcea4d43d6faa1714782661af7a4
Qwikle <qwikle.ziko@gmail.com>
2023-04-14 10:44:21 +0200

    feat: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./resources/**/*.{edge,js,ts,jsx,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+
+  daisyui: {
+    themes: ['cupcake', 'luxury'],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    darkTheme: 'luxury',
+  },
+}
