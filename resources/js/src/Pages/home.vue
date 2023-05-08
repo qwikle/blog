8cf4f41391f53a4b0fc5581cc50f24fbf8314e32
Qwikle <qwikle.ziko@gmail.com>
2023-05-08 19:44:37 +0200

    feat: WIP home page

--- resources/js/src/Pages/home.vue
+++ resources/js/src/Pages/home.vue
@@ -1,9 +1,11 @@
 <template>
-  <Base>
-    <p>Home</p>
-  </Base>
+  <Base> </Base>
 </template>
 <script setup>
 import Base from '@/Layouts/Base.vue'
+import { usePage } from '@inertiajs/vue3'
+
+const page = usePage()
+const user = page.props.user
 </script>
 <style scoped></style>
