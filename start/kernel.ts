a181c611f50bed7d9ce1fc4076428bd5e02f0ef8
Qwikle <qwikle.ziko@gmail.com>
2023-05-08 18:22:14 +0200

    feat: add guest middleware

--- start/kernel.ts
+++ start/kernel.ts
@@ -45,4 +45,5 @@ Server.middleware.register([
 Server.middleware.registerNamed({
   auth: () => import('App/Middleware/Auth'),
   admin: () => import('App/Middleware/Admin'),
+  guest: () => import('App/Middleware/Guest'),
 })
