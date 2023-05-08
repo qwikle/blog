8e68e433870f3c53d7564895e1663c0601ad3a96
Qwikle <qwikle.ziko@gmail.com>
2023-05-08 19:44:07 +0200

    refactor: add role to user load

--- start/inertia.ts
+++ start/inertia.ts
@@ -15,7 +15,11 @@ Inertia.share({
     return ctx.session.flashMessages.get('errors')
   },
   params: ({ params }) => params,
-  user: ({ auth }) => auth?.user,
+  user: async ({ auth }) => {
+    let user = auth.user
+    await user?.load('role')
+    return user
+  },
   error: ({ session }) => session.flashMessages.get('error'),
   success: ({ session }) => session.flashMessages.get('success'),
 }).version(() => Inertia.manifestFile('public/assets/manifest.json'))
