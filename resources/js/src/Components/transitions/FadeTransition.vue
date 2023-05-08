17d0733cd11acc0cda562d17df6c91e7b24091c4
Qwikle <qwikle.ziko@gmail.com>
2023-05-08 18:27:37 +0200

    feat: add alert component

---
+++ resources/js/src/Components/transitions/FadeTransition.vue
@@ -0,0 +1,16 @@
+<template>
+  <Transition name="fade" mode="out-in" appear>
+    <slot></slot>
+  </Transition>
+</template>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
